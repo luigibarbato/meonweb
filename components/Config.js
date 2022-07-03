7d747c278ed9ab58ae53d725171c877ec7c46696
Luigi Barbato <luigi.barbato@sighup.io>
2022-07-03 20:25:25 +0200

    feat: add some seo content

--- components/Config.js
+++ components/Config.js
@@ -1,21 +1,21 @@
 const Sections = [
   {
     name: "Home",
-    description: "Home page",
+    description: "Is the home page of the website",
     url: "/",
     colors: ["#A9E775", "#5A45FF", "#5A45FF"],
     radialBackground: true,
   },
   {
     name: "Work",
-    description: "Home page",
+    description: "Is the page where you can see my work",
     url: "/Work",
     colors: ["#D3ED10", "#FF7A45", "#FF7A45"],
     radialBackground: true,
   },
   {
     name: "Loving",
-    description: "Home page",
+    description: "Is the page where you can see my interests",
     url: "/Loving",
     colors: ["#e732bd", "#1b00e8", "#1b00e8"],
     radialBackground: true,
