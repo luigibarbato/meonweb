347f047a82c51ee6c5b3822cb7a82c897aeb201e
Luigi Barbato <luigi.barbato@sighup.io>
2022-07-03 21:13:26 +0200

    chore: remove posthog support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import '../styles/globals.css'
-import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { useState } from "react";
 import Nav from '../components/Navbar'
 import { Config } from '../components/Config'
 import { Links } from '../components/Navbar/NavbarLink/Types/Link'
-import { usePostHog } from 'next-use-posthog'
 
 // TODO: Improve general colors,text etc.
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
@@ -36,8 +35,6 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
       }
     })
   })
-
-  usePostHog(process.env.POSTHOG_API || "", { api_host: 'https://app.posthog.com' })
 
   return (
     <section className="text-gray-500 h-screen md:flex md:flex-col stepper overflow-y-scroll md:overflow-hidden">
